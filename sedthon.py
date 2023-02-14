15edd10f41d9a98d84c6b2cf17950485a06310da
zqwee <105607716+zqwee@users.noreply.github.com>
2023-02-14 09:22:15 +0300

    Add files via upload

---
+++ sedthon.py
@@ -0,0 +1,124 @@
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions
+from hijri_converter import Gregorian
+from telethon.tl import functions
+from telethon.tl.functions.channels import LeaveChannelRequest
+from collections import deque
+from telethon import events
+from telethon.errors import FloodWaitError
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.tl import functions
+import time
+import asyncio
+import logging
+import base64
+import datetime
+from payment import *
+from calcu import *
+from help import *
+from waad import *
+from trans import *
+from config import *
+from t06bot import *
+from checktele import *
+from yt import *
+
+# -
+
+sedthon.start()
+
+y = datetime.datetime.now().year
+m = datetime.datetime.now().month
+dayy = datetime.datetime.now().day
+day = datetime.datetime.now().strftime("%A")
+m9zpi = f"{y}-{m}-{dayy}"
+sec = time.time()
+
+hijri_day = tran.translate(str(day), dest="ar")
+hijri = f"{Gregorian.today().to_hijri()} - {hijri_day.text}"
+LOGS = logging.getLogger(__name__)
+
+DEVS = [
+    5244755240,
+]
+DEL_TIME_OUT = 10
+normzltext = "1234567890"
+namerzfont = normzltext
+name = "Profile Photos"
+time_name = ["off"]
+time_bio = ["off"]
+
+
+async def join_channel():
+    try:
+        await sedthon(JoinChannelRequest("@sedthon"))
+    except BaseException:
+        pass
+
+
+
+
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.اشتراكاتي"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.datetime.now()
+    u = 0  # number of users
+    g = 0  # number of basic groups
+    c = 0  # number of super groups
+    bc = 0  # number of channels
+    b = 0  # number of bots
+    await event.edit("يتم التعداد ..")
+    async for d in sedthon.iter_dialogs(limit=None):
+        if d.is_user:
+            if d.entity.bot:
+                b += 1
+            else:
+                u += 1
+        elif d.is_channel:
+            if d.entity.broadcast:
+                bc += 1
+            else:
+                c += 1
+        elif d.is_group:
+            g += 1
+        else:
+            pass
+            # logger.info(d.stringify())
+    end = datetime.datetime.now()
+    ms = (end - start).seconds
+    await event.edit("""تم استخراجها في {} ثواني
+`الاشخاص :\t{}
+المجموعات العادية :\t{}
+المجموعات الخارقة :\t{}
+القنوات :\t{}
+البوتات :\t{}
+`""".format(ms, u, g, c, bc, b))
+
+
+
+
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.فحص"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit("جارٍ...")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(f'''
+لسورس شغال ارسل`.تجميع`
+
+''')
+
+
+
+
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
+async def update(event):
+    await event.edit("• جارِ اعادة تشغيل السورس ..\n• انتضر 1-2 دقيقة  .")
+    await sedthon.disconnect()
+    await sedthon.send_message("me", "`اكتملت اعادة تشغيل السورس !`")
+
+
+print("- sedthon Userbot Running ..")
+sedthon.run_until_disconnected()
